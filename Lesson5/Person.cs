59a87754b21fd5482f54f25fcf1d16ad5de93df8
VakhnenkoIlya <mazerfaker@list.ru>
2021-04-16 23:13:28 +0300

    Lesson5

---
+++ Lesson5/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lesson5
+{
+    class Person
+    {
+        public string Name { get; }
+
+        public string Position { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public int Salary { get; set; }
+
+        public int Age { get; set; }
+
+        public void GetWrite()
+        {
+            Console.WriteLine(Name, Position, Email, Phone, Salary, Age);
+        }
+        public Person(string name, string position, string email, string phone, int salary, int age)
+        {
+            Name = name;
+            Position = position;
+            Email = email;
+            Phone = phone;
+            Salary = salary;
+            Age = age;
+
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Lesson5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using System.IO;
+namespace Lesson5
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1");
+
+            string filename = "startup.txt";
+            File.WriteAllText(filename, "Этот файл введен с клавиатуры \n");
+
+            Console.WriteLine("Задание 2");
+
+            File.AppendAllText(filename, DateTime.Now.ToString());
+
+            Console.WriteLine("Задание 3");
+
+            byte[] aray = { 1, 3, 4, 9 };
+            File.WriteAllBytes("bytes.bin", aray);
+
+
+            Console.WriteLine("Задание 4");
+
+            Person[] persArray = new Person[5];
+            persArray[0] = new Person("Sidorov Ivan", "manager", "si@mail.ru", "+79001111111", 25000, 25);
+            persArray[1] = new Person("Petrov Petr", "engineer", "PP@mail.ru", "+79002222222", 30000, 30);
+            persArray[2] = new Person("Ivanov Ivan", "operator", "ii@mail.ru", "+79003333333", 20000, 45);
+            persArray[3] = new Person("Fedorov Fedor", "operator", "ff@mail.ru", "+79004444444", 20000, 35);
+            persArray[4] = new Person("Egorov Egor", "director", "ee@mail.ru", "+79005555555", 50000, 50);
+
+            foreach (Person pers in persArray)
+            {
+                if (pers.Age > 40)
+                {
+                    pers.GetWrite();
+                }
+            
+            }
+
+
+
+
+
+
+        }
+    }
+}
