5480a17146f0004b911da7bdf48ae4fcde755c41
VakhnenkoIlya <mazerfaker@list.ru>
2021-04-28 18:39:03 +0300

    Lesson8

---
+++ Lesson8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace Lesson8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.PrtMsg();
+        }
+    }
+}
